09fc2e8676d26a4b646400bc7f4cdf059ac905a6
Gabriele Corti <borntofrappe@gmail.com>
2025-05-01 21:20:08 +0200

    button styles

--- sveltekit/src/routes/Add.svelte
+++ sveltekit/src/routes/Add.svelte
@@ -4,7 +4,7 @@
 
 <form use:enhance method="POST" action="?/add">
 	<input id="text" name="text" type="text" placeholder="Add new item" required />
-	<button aria-label="Add new item">
+	<button class="svg-button" aria-label="Add new item">
 		<svg viewBox="0 -960 960 960" width="1em" height="1em">
 			<path fill="currentColor" d="M440-440H200v-80h240v-240h80v240h240v80H520v240h-80v-240Z" />
 		</svg>
@@ -37,6 +37,7 @@
     }
 
     button {
+      --button-size: var(--_button-size);
       position: absolute;
       inset-block-start: 50%;
       inset-inline-end: var(--_inline-padding);

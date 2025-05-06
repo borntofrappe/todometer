7e0d8f105a26082fdaac5d7b76b84a6d36f795e5
Gabriele Corti <borntofrappe@gmail.com>
2025-05-06 07:45:50 +0200

    Header.svelte

--- todometer/src/routes/+page.svelte
+++ todometer/src/routes/+page.svelte
@@ -1,1 +1,19 @@
-<h1>todometer</h1>
+<script lang="ts">
+  import Header from "./Header.svelte";
+</script>
+
+<div class="root">
+  <Header />
+</div>
+
+<style>
+  .root {
+    padding: var(--size-3) var(--size-2);
+    max-inline-size: var(--size-content-3);
+    margin-inline: auto;
+
+    > :global(* + *) {
+      margin-block-start: var(--size-5);
+    }
+  }
+</style>

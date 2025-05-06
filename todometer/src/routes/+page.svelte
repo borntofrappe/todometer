d6021ce9b31c691bd1cbc4959993810dca51e093
Gabriele Corti <borntofrappe@gmail.com>
2025-05-06 08:09:15 +0200

    add action
    preferred a  value instead of surfacing the text input from the component

--- todometer/src/routes/+page.svelte
+++ todometer/src/routes/+page.svelte
@@ -3,11 +3,14 @@
 
   import Header from "./Header.svelte";
   import Meter from "./Meter.svelte";
+  import Add from "./actions/Add.svelte";
 
   let todos: Todo[] = $state([]);
   let complete = $derived(todos.filter((d) => d.status === "complete"));
   let paused = $derived(todos.filter((d) => d.status === "paused"));
   let pending = $derived(todos.filter((d) => d.status === "pending"));
+
+  let value = $state("");
 
   $effect(() => {
     Database.load("sqlite:todos.db").then((db) => {
@@ -16,12 +19,32 @@
       });
     });
   });
+
+  const addTodo = async () => {
+    if (value === "") return;
+
+    const db = await Database.load("sqlite:todos.db");
+    await db.execute("INSERT INTO todos (text) VALUES ($1)", [value]);
+    todos = (await db.select("SELECT * FROM todos")) as Todo[];
+
+    value = "";
+  };
+
+  $inspect(todos);
 </script>
 
 <div class="root">
   <Header />
 
   <Meter values={[complete.length, paused.length, pending.length]} />
+
+  <Add
+    bind:value
+    onsubmit={(event: SubmitEvent) => {
+      event.preventDefault();
+      addTodo();
+    }}
+  />
 </div>
 
 <style>
---
+++ todometer/src/routes/actions/Add.svelte
@@ -0,0 +1,50 @@
+<script lang="ts">
+  let { value = $bindable(), onsubmit } = $props();
+</script>
+
+<form {onsubmit}>
+  <input type="text" placeholder="Add new item" bind:value />
+  <button class="svg-button" aria-label="Add new item">
+    <svg viewBox="0 -960 960 960" width="1em" height="1em">
+      <path
+        fill="currentColor"
+        d="M440-440H200v-80h240v-240h80v240h240v80H520v240h-80v-240Z"
+      />
+    </svg>
+  </button>
+</form>
+
+<style>
+  form {
+    position: relative;
+    --_inline-padding: var(--size-3);
+    --_button-size: 2em;
+
+    input {
+      font-size: var(--font-size-2);
+      inline-size: 100%;
+      color: var(--text-1);
+      background: var(--surface-1);
+      border: none;
+      padding: var(--size-3) var(--_inline-padding);
+      padding-inline-end: calc(var(--_inline-padding) + var(--_button-size));
+
+      &::placeholder {
+        color: var(--text-3);
+        font-weight: 300;
+      }
+
+      &:focus {
+        outline: none;
+      }
+    }
+
+    button {
+      --button-size: var(--_button-size);
+      position: absolute;
+      inset-block-start: 50%;
+      inset-inline-end: var(--_inline-padding);
+      translate: 0% -50%;
+    }
+  }
+</style>

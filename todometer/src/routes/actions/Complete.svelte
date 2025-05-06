c5dcf1f793f2e92245b99431d534bcde1bbf46a3
Gabriele Corti <borntofrappe@gmail.com>
2025-05-06 08:17:55 +0200

    missing actions
    follow the scheme of Reset.svelte where the components essentially do something established in the parent scope; Toggle.svelte has an additional property to highlight whether the button is pressed or not

---
+++ todometer/src/routes/actions/Complete.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  let { onclick } = $props();
+</script>
+
+<button {onclick} class="svg-button" aria-label="Complete todo">
+  <svg viewBox="0 -960 960 960" width="1em" height="1em">
+    <path
+      fill="currentColor"
+      d="M382-240 154-468l57-57 171 171 367-367 57 57-424 424Z"
+    />
+  </svg>
+</button>
+
+<style>
+  button {
+    color: var(--g-2, lightgreen);
+  }
+</style>
---
+++ todometer/src/routes/actions/Delete.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  let { onclick } = $props();
+</script>
+
+<button {onclick} class="svg-button" aria-label="Delete todo">
+  <svg viewBox="0 -960 960 960" width="1em" height="1em">
+    <path
+      fill="currentColor"
+      d="m256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z"
+    />
+  </svg>
+</button>
+
+<style>
+  button {
+    color: var(--r-2, lightcoral);
+  }
+</style>

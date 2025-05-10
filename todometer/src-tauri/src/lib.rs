0b969a088083c6b0fa89926f1eea64f160296636
Gabriele Corti <borntofrappe@gmail.com>
2025-05-10 09:55:36 +0200

    single instance
    focus on the opened, non-minimised app

--- todometer/src-tauri/src/lib.rs
+++ todometer/src-tauri/src/lib.rs
@@ -1,4 +1,5 @@
 use tauri_plugin_sql::{Migration, MigrationKind};
+use tauri::{Manager};
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
@@ -13,7 +14,19 @@ pub fn run() {
         kind: MigrationKind::Up,
     }];
 
-    tauri::Builder::default()
+    let mut builder = tauri::Builder::default();
+
+    #[cfg(desktop)]
+    {
+        builder = builder.plugin(tauri_plugin_single_instance::init(|app, _args, _cwd| {
+            let _ = app
+                .get_webview_window("main")
+                .expect("no main window")
+                .set_focus();
+        }));
+    }
+
+    builder
         .plugin(tauri_plugin_os::init())
         .plugin(
             tauri_plugin_sql::Builder::new()

4590dc0c9b10a1cd2386ac643d10575687ee7fb1
Gabriele Corti <borntofrappe@gmail.com>
2025-05-20 10:53:18 +0200

    desktop config
    include desktop plugins, single instance and positioner, in the block running for desktop environments

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,5 +1,3 @@
-use tauri::Manager;
-use tauri_plugin_positioner::{Position, WindowExt};
 use tauri_plugin_sql::{Migration, MigrationKind};
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
@@ -18,12 +16,23 @@ pub fn run() {
     let mut builder = tauri::Builder::default();
     #[cfg(desktop)]
     {
-        builder = builder.plugin(tauri_plugin_single_instance::init(|app, _args, _cwd| {
-            let _ = app
-                .get_webview_window("main")
-                .expect("no main window")
-                .set_focus();
-        }));
+        use tauri::Manager;
+        use tauri_plugin_positioner::{Position, WindowExt};
+
+        builder = builder
+            .plugin(tauri_plugin_single_instance::init(|app, _args, _cwd| {
+                let _ = app
+                    .get_webview_window("main")
+                    .expect("no main window")
+                    .set_focus();
+            }))
+            .plugin(tauri_plugin_positioner::init())
+            .setup(|app| {
+                let win = app.get_webview_window("main").unwrap();
+                let _ = win.as_ref().window().move_window(Position::Center);
+                let _ = win.show();
+                Ok(())
+            });
     }
 
     builder
@@ -34,16 +43,6 @@ pub fn run() {
                 .add_migrations("sqlite:todos.db", migrations)
                 .build(),
         )
-        .plugin(tauri_plugin_positioner::init())
-        .setup(|app| {
-            #[cfg(desktop)]
-            {
-                let win = app.get_webview_window("main").unwrap();
-                let _ = win.as_ref().window().move_window(Position::Center);
-                let _ = win.show();
-            }
-            Ok(())
-        })
         .plugin(tauri_plugin_opener::init())
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
